2ec039b985bf12b4b8a4ddaa81d47bc1d54915ab
rahulraj0402 <91648660+rahulraj0402@users.noreply.github.com>
2023-05-28 17:26:40 +0000

    solved goal-parser-interpretation

---
+++ easy/1678.java
@@ -0,0 +1,23 @@
+//https://leetcode.com/problems/goal-parser-interpretation/
+class Solution {
+    public String interpret(String str) {
+
+        StringBuilder sb = new StringBuilder("");
+
+        for(int i = 0 ; i<str.length() ; i++){
+
+            if(str.charAt(i)=='G'){
+                sb.append("G");
+            }else if(str.charAt(i)=='('){
+                if(str.charAt(i+1)==')'){
+                    sb.append("o");
+                }else{
+                sb.append("al");
+            }
+            }
+
+        }
+        return sb.toString();
+        
+    }
+}
